a62690870c07a1374fcf890f3c0628894597f909
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-08-06 05:07:59 +0900

    멤버 도메인 로직 개발

--- src/main/java/com/codeit/splearn/domain/Member.java
+++ src/main/java/com/codeit/splearn/domain/Member.java
@@ -1,8 +1,13 @@
 package com.codeit.splearn.domain;
 
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.Assert;
+
+import java.util.Objects;
 
 @Getter
+@ToString
 public class Member {
     private String email;
 
@@ -13,12 +18,22 @@ public class Member {
     private MemberStatus status;
 
     public Member(String email, String nickname, String passwordHash) {
-        this.email = email;
-        this.nickname = nickname;
-        this.passwordHash = passwordHash;
+        this.email = Objects.requireNonNull(email);
+        this.nickname = Objects.requireNonNull(nickname);
+        this.passwordHash = Objects.requireNonNull(passwordHash);
         this.status = MemberStatus.PENDING;
     }
 
 
+    public void activate() {
+        Assert.state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
 
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    public void deactivate() {
+        Assert.state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+        this.status = MemberStatus.DEACTIVATED;
+    }
 }
--- src/test/java/com/codeit/splearn/domain/MemberTest.java
+++ src/test/java/com/codeit/splearn/domain/MemberTest.java
@@ -3,6 +3,7 @@ package com.codeit.splearn.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTest {
 
@@ -12,5 +13,47 @@ class MemberTest {
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
+    @Test
+    void constructorNullCheck() {
+        assertThatThrownBy(() -> new Member(null, "Toby", "secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("toby", "Toby", "secret");
+        member.activate();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        var member = new Member("toby", "Toby", "secret");
+        member.activate();
+        assertThatThrownBy(member::activate).isInstanceOf(IllegalStateException.class);
+
+    }
+    
+    @Test
+    void deactivate() {
+        var member = new Member("toby", "Toby", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("toby", "Toby", "secret");
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+    }
 
 }
