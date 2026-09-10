ece436362b7693689d76f4683bad4be5b8179ea6
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-09-10 09:40:11 +0900

    feat: 엔티티의 식별자와 JPA 엔티티

--- src/test/java/com/codeit/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/com/codeit/splearn/application/required/MemberRepositoryTest.java
@@ -2,15 +2,16 @@ package com.codeit.splearn.application.required;
 
 import com.codeit.splearn.domain.Member;
 import com.codeit.splearn.domain.MemberFixture;
-import com.codeit.splearn.domain.MemberRegisterRequest;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static com.codeit.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -24,17 +25,22 @@ class MemberRepositoryTest {
     @Test
     @DisplayName("멤버 생성 테스트")
     void createMember() {
-        // given
         Member member = Member.register(createMemberRegisterRequest(),  MemberFixture.createPasswordEncoder());
 
-        // when
         memberRepository.save(member);
 
         assertThat(member.getId()).isNotNull();
+
         entityManager.flush();
+    }
 
-        // then
+    @Test
+    void duplicateEmailFail() {
+        Member member = Member.register(createMemberRegisterRequest(), MemberFixture.createPasswordEncoder());
+        memberRepository.save(member);
 
-
+        Member member2 = Member.register(createMemberRegisterRequest(), MemberFixture.createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
